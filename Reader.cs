489e9c5acfd1410c97dae1162a5d69d28cd2cb8c
Nikita <draco1997pst@gmail.com>
2024-02-24 20:36:05 +0200

    Добавьте файлы проекта.

---
+++ Reader.cs
@@ -0,0 +1,50 @@
+﻿using generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Читатель(Reader):
+//Свойства: Имя, Номер читательского билета, Список взятых книг.
+//Методы: Взять книгу, Вернуть книгу.
+book book1 = new book("tven ", "jony","Horror ", 3456);
+Reader<long> reader = new Reader<long>("joe", 356765);
+
+
+reader.AddBook(book1);
+reader.Info();
+reader.DeletBook(book1);
+reader.Info();
+
+
+namespace generic
+{
+    internal class Reader<T>
+    {
+        public string Name { get; set; }
+        public T NumBilet { get; set; }
+        List<book> books { get; set; }
+
+        public Reader(string name, T num)
+        {
+            Name = name;
+            NumBilet = num;
+            books = new List<book>();
+        }
+
+        public void AddBook(book book) {  books.Add(book); Console.WriteLine($"{book.Title} ADD"); }
+
+        public void DeletBook(book book) { books.Remove(book); Console.WriteLine($"{book.Title} dell"); }
+
+        public void Info() 
+        {
+            foreach (var i in books)
+            {
+                Console.WriteLine(i.Title);
+            }
+        }
+        
+    }
+}
---
+++ book.cs
@@ -0,0 +1,36 @@
+﻿using generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Книга (Book):
+//Свойства: Название, Автор, Год издания, Жанр, Уникальный идентификатор (ISBN).
+//Методы: Конструктор для инициализации книги.
+
+
+
+
+
+namespace generic
+{
+    internal class book
+    {
+        public string Title { get; }
+        public string Autor { get; }
+        public string Janr { get; }
+        public double ISBN { get; }
+
+        public book(string title, string autor, string janr, double iSBN)
+        {
+            Title = title;
+            Autor = autor;
+            Janr = janr;
+            ISBN = iSBN;
+        }   
+    }
+}
+
+
+
